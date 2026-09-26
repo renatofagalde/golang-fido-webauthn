e6afb16794dbdee39f2dd0acfa1bdb23a8dc294f
Renato Fagalde <renato@likwi.com.br>
2026-09-26 17:29:17 -0300

    feat(fido): adicionar mapeamento de erros do domínio para respostas HTTP

---
+++ internal/fido/infrastructure/http/errors.go
@@ -0,0 +1,23 @@
+package http
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/renatofagalde/golang-fido-webauthn/internal/fido/domain"
+	"github.com/renatofagalde/golang-fido-webauthn/pkg/writeerror"
+)
+
+func writeDomainError(c *gin.Context, err error) {
+	switch {
+	case errors.Is(err, domain.ErrInvalidInput):
+		writeerror.JSON(c, http.StatusBadRequest, err.Error())
+	case errors.Is(err, domain.ErrUsernametaken):
+		writeerror.JSON(c, http.StatusConflict, err.Error())
+	case errors.Is(err, domain.ErrUserNotFound):
+		writeerror.JSON(c, http.StatusNotFound, err.Error())
+	default:
+		writeerror.JSON(c, http.StatusInternalServerError, "internal error")
+	}
+}
